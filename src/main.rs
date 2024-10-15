31243e98166668d3322c603d4a3d87541fe481c4
Horc-zh <127604084+Horc-zh@users.noreply.github.com>
2024-10-15 17:15:49 +0800

    Testing Part I

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 extern crate lazy_static;
 extern crate spin;
+extern crate uart_16550;
 extern crate volatile;
+extern crate x86_64;
+
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -14,18 +20,43 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// static HELLO: &[u8] = b"HELLO WORLD";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("hello");
+    println!("Hello rust_os");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
 }
